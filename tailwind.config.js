07937e9df5ce08334fa47fc2181b4cc7c0acefc6
ishan <megaoceans.official@gmail.com>
2024-09-26 17:47:48 +0530

    resolved broken next app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const { default: WhyUs } = require("./app/client/whyUs/whyUs");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,7 +5,6 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-
   theme: {
     extend: {
       backgroundImage: {
