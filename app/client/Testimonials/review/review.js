828371a3c97fc807c8986e68175ce712126ef1fa
ishan <megaoceans.official@gmail.com>
2025-01-28 12:08:42 +0530

    fix(m) img tag to next/Image

--- app/client/Testimonials/review/review.js
+++ app/client/Testimonials/review/review.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 export default function Review({ company, title, desc, src, name }) {
   return (
     <div className="w-review h-review rounded-3xl bg-terinary px-8 py-10 flex flex-col items-start justify-between">
@@ -5,7 +6,13 @@ export default function Review({ company, title, desc, src, name }) {
       <p className="text-white text-2xl font-semibold">{title}</p>
       <p className="text-white text-sm font-medium">{desc}</p>
       <div className="flex items-center gap-6 w-full h-12 ">
-        <img src={src} alt={src} className="w-12 h-12 rounded-full" />
+        <Image
+          src={src}
+          alt={src}
+          className="w-12 h-12 rounded-full"
+          width={48}
+          height={48}
+        />
         <div className="flex items-start flex-col justify-between">
           <p className="text-white text-base font-semibold">{name}</p>
           <p className="text-white text-lg font-light">{company}</p>
