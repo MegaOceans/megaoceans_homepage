574eaa4079d626f95d4902a7623487cb5bb9bc0d
ishan <megaoceans.official@gmail.com>
2025-01-29 15:17:55 +0530

    feat(m) added a new mobile app image

--- app/client/templates/templates.js
+++ app/client/templates/templates.js
@@ -8,6 +8,7 @@ export default function Templates() {
     "/assets/templates/img1.png",
     "/assets/templates/easyformations.jpeg",
     "/assets/templates/easyformations2.jpeg",
+    "/assets/templates/mob-app.png",
   ];
   return (
     <div className="w-full flex flex-col items-center gap-12 text-white">
