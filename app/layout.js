006f2cd54d0260431a86a816f5eee2b802c90bcd
Ishan Dev <142209359+ishan-MO@users.noreply.github.com>
2025-06-06 17:18:05 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-XM1H5BVSYF"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-XM1H5BVSYF');
+    </script>
     </html>
   );
 }
